ae78178093a71a2f4c027b5bc2a214eeb9d90017
Jyothiprakashg-dev <jyothiprakashg18@gmail.com>
2025-07-26 16:17:32 +0000

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ padding: '10px', backgroundColor: '#f5f5f5' }}>
+      <h1>🍕 Foodie - Online Food Delivery</h1>
+    </header>
+  );
+};
+
+export default Header;
